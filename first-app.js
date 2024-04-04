1e5507b13f0ab2c76850495d15f97199c28ce637
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-04 20:36:55 +0200

    Document created

--- first-app.js
+++ first-app.js
@@ -1,8 +1,5 @@
 const fs = require("fs");
 fs.writeFileSync("Hello.txt", "Hello from Node.JS again");
 
-// test commit new
-// git commit number 1
-// git commit number 2
-// git commit number 3
+
 
