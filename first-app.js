6e72934f6d68ab311c81a661d357d4c4dedd23cb
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-04 20:22:18 +0200

    Second commit

--- first-app.js
+++ first-app.js
@@ -1,5 +1,7 @@
-const fs = require('fs')
-fs.writeFileSync('Hello.txt', 'Hello from Node.JS again')
+const fs = require("fs");
+fs.writeFileSync("Hello.txt", "Hello from Node.JS again");
 
 // test commit new
 // git commit number 1
+// git commit number 2
+// git
