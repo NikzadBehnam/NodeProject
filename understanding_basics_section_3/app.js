94c9c43f9b5ca082026bb7b206cb6eaa8f1a1c3c
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-19 19:54:18 +0200

    33.Redirecting Requests

--- understanding_basics_section_3/app.js
+++ understanding_basics_section_3/app.js
@@ -1,8 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
+    console.log(url);
     res.write("<html>");
     res.write("<head><title>My first page</title></head>");
     res.write(
@@ -12,6 +15,12 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
 
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY 123456");
+    res.statusCode = 302;
+    res.setHeader("Location", "/files");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My first Node server response</title></head>");
