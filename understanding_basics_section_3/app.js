efeb0fe6c1927c2658d0266fbe22d4a821c303a2
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-21 17:52:33 +0200

    34-Parsing Request Bodies

--- understanding_basics_section_3/app.js
+++ understanding_basics_section_3/app.js
@@ -4,29 +4,38 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
+
   if (url === "/") {
-    console.log(url);
     res.write("<html>");
     res.write("<head><title>My first page</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text' /><button type='submit'>Submit</button></form></body>"
+      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Submit</button></form></body>"
     );
     res.write("</html>");
     return res.end();
   }
 
   if (url === "/message" && method === "POST") {
-    fs.writeFileSync("message.txt", "DUMMY 123456");
-    res.statusCode = 302;
-    res.setHeader("Location", "/files");
-    return res.end();
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message); // Write file here
+      res.statusCode = 302;
+      res.setHeader("Location", "/");
+      return res.end();
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My first Node server response</title></head>");
+    res.write("<body><h1>Hello World !!!</h1></body>");
+    res.write("</html>");
+    res.end();
   }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first Node server response</title></head>");
-  res.write("<body><h1>Hello World !!!</h1></body>");
-  res.write("</html>");
-  res.end();
 });
 
 server.listen(3000);
