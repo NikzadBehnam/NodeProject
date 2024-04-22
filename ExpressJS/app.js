afb05f67e5d6667a6ebf6c3364b6a727c3f8feec
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-22 18:33:49 +0200

    60-Adding Middleware

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -3,6 +3,14 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+app.use((res, req, next) => {
+  console.log("In the middleware ...");
+  next();
+});
+
+app.use((res, req, next) => {  
+  console.log("In the next middleware ...");
+});
 const server = http.createServer(app);
 
 server.listen(3000);
