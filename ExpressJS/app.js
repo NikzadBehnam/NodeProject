87711b288ef0f998300ade8e8e834272e30b8418
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-27 10:39:51 +0200

    64. Parsing Incoming Requests

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,14 +1,24 @@
-const http = require("http");
+// import express from "express";
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+// body parser is activated
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (req, res, next) => {
-  res.send("<h1>Add product ...!</h1>");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title' /><button type='submit'>Add product</button></form>"
+  );
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  res.send("<h1>Hello Express ...!</h1>");
+  res.send("<h1>Hello this is ExpressJS</h1>");
 });
 
 app.listen(3000);
