f960145f210068ce1c2e29fc4850fbfcee59ac6e
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-28 20:56:41 +0200

    70. Serving HTML Pages

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -1,5 +1,6 @@
 // import express from "express";
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -9,10 +10,13 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
-app.use("/shop", shopRoutes);
+app.use("/", shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found ...</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+  // res.status(404).send("<h1>Page Not Found ...</h1>");
 });
 
 app.listen(3000);
