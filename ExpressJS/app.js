1162c46909e89eeeed324dd68c6ee8f19598bf27
NikzadBehnam <nikzad.behnam1992@gmail.com>
2024-04-30 17:31:39 +0200

    75. Serving Files Statically

--- ExpressJS/app.js
+++ ExpressJS/app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const rootDir = require("./util/path");
+app.use(express.static(path.join(rootDir, "public")));
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
